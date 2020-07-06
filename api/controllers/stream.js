54a30a5285c5dd32aa6505d183da10fa52d86381
tothadi <Oakerator66>
2020-07-06 11:25:37 +0200

    ffmpeg in client request

--- api/controllers/stream.js
+++ api/controllers/stream.js
@@ -1,15 +1,6 @@
 const child_process = require('child_process'),
-    path = process.env.StreamURI,//'./cars.mp4'//
-    ffmpeg = child_process.spawn('ffmpeg', [
-        '-reorder_queue_size', '5', '-rtsp_transport', 'tcp',
-        '-i', path, '-framerate', '25',
-        '-an', '-c:v', 'copy', '-f', 'mp4', '-movflags', '+frag_keyframe+empty_moov+default_base_moof', '-fflags', '+igndts', '-metadata', 'title="media source extensions"', 'pipe:1'
-    ])
+    path = process.env.StreamURI//'./cars.mp4'//
 
-ffmpeg.stderr.setEncoding('utf8');
-ffmpeg.stderr.on('data', (data) => {
-    console.log(data)
-})
 
 
 
@@ -19,6 +10,17 @@ module.exports.sendStream = function (req, res) {
         'Access-Control-Allow-Origin': '*',
         'Connection': 'Keep-Alive',
         'Content-Type': 'video/mp4'
+    })
+
+    const ffmpeg = child_process.spawn('ffmpeg', [
+        '-fflags', '+igndts', '-reorder_queue_size', '5', '-rtsp_transport', 'tcp',
+        '-i', path, '-framerate', '25',
+        '-an', '-c:v', 'copy', '-f', 'mp4', '-movflags', '+frag_keyframe+empty_moov+default_base_moof', '-metadata', 'title="media source extensions"', 'pipe:1'
+    ])
+
+    ffmpeg.stderr.setEncoding('utf8');
+    ffmpeg.stderr.on('data', (data) => {
+        console.log(data)
     })
 
     ffmpeg.stdout.pipe(res)
