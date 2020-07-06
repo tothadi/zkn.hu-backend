d2947e8af22e55bc1da39179a7a357c4bc529bcd
tothadi <Oakerator66>
2020-07-06 08:10:20 +0200

    ignore dts

--- api/controllers/stream.js
+++ api/controllers/stream.js
@@ -1,5 +1,17 @@
 const child_process = require('child_process'),
-    path = process.env.StreamURI//'./cars.mp4'//
+    path = process.env.StreamURI,//'./cars.mp4'//
+    ffmpeg = child_process.spawn('ffmpeg', [
+        '-reorder_queue_size', '5', '-rtsp_transport', 'tcp',
+        '-i', path, '-framerate', '25',
+        '-an', '-c:v', 'copy', '-f', 'mp4', '-movflags', '+frag_keyframe+empty_moov+default_base_moof+igndts', '-metadata', 'title="media source extensions"', 'pipe:1'
+    ])
+
+ffmpeg.stderr.setEncoding('utf8');
+ffmpeg.stderr.on('data', (data) => {
+    console.log(data)
+})
+
+
 
 module.exports.sendStream = function (req, res) {
 
@@ -9,20 +21,6 @@ module.exports.sendStream = function (req, res) {
         'Content-Type': 'video/mp4'
     })
 
-    // transcode rtsp input from ip-cam to mp4 file format (video: h.264 | audio: aac)
-    let ffmpeg = child_process.spawn('ffmpeg', [
-        '-reorder_queue_size', '5', '-rtsp_transport', 'tcp',
-        '-i', path, '-framerate', '25',
-        '-an', '-c:v', 'copy', '-f', 'mp4', '-movflags', '+frag_keyframe+empty_moov+default_base_moof', '-metadata', 'title="media source extensions"', 'pipe:1'
-    ])
-
-
-    // redirect transcoded ip-cam stream to http response
     ffmpeg.stdout.pipe(res)
 
-    // error logging
-    ffmpeg.stderr.setEncoding('utf8');
-    ffmpeg.stderr.on('data', (data) => {
-        console.log(data)
-    })
 }
