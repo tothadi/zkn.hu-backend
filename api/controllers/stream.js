461b73b232abbddb5aeae47033dd33013e4e07b4
tothadi <Oakerator66>
2020-07-07 10:53:09 +0200

    ffmpeg out of request

--- api/controllers/stream.js
+++ api/controllers/stream.js
@@ -1,6 +1,11 @@
 const child_process = require('child_process'),
     path = process.env.StreamURI//'./cars.mp4'//
-
+const ffmpeg = child_process.spawn('ffmpeg', [
+        '-fflags', '+igndts', '-reorder_queue_size', '5', '-max_delay', '5', '-rtsp_transport', 'tcp',
+        '-i', path, '-framerate', '25',
+        '-an', '-c:v', 'copy', '-f', 'mp4', '-movflags', '+frag_keyframe+empty_moov+default_base_moof', '-metadata', 'title="media source extensions"', 'pipe:1'
+    ])
+    
 module.exports.sendStream = function (req, res) {
 
     res.writeHead(200, {
@@ -9,11 +14,7 @@ module.exports.sendStream = function (req, res) {
         'Content-Type': 'video/mp4'
     })
 
-    const ffmpeg = child_process.spawn('ffmpeg', [
-        '-fflags', '+igndts', '-reorder_queue_size', '5', '-max_delay', '5', '-rtsp_transport', 'tcp',
-        '-i', path, '-framerate', '25',
-        '-an', '-c:v', 'copy', '-f', 'mp4', '-movflags', '+frag_keyframe+empty_moov+default_base_moof', '-metadata', 'title="media source extensions"', 'pipe:1'
-    ])
+    
 
     ffmpeg.stderr.setEncoding('utf8');
     ffmpeg.stderr.on('data', (data) => {
