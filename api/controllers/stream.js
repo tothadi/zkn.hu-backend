8fa1e50bf2437a8a54e40ae3415f8636c06b370b
tothadi <Oakerator66>
2020-07-07 13:08:35 +0200

    added thread type

--- api/controllers/stream.js
+++ api/controllers/stream.js
@@ -1,6 +1,6 @@
 const child_process = require('child_process'),
     path = process.env.StreamURI//'./cars.mp4'//
-
+//ffmpeg = require('fluent-ffmpeg')
 
 module.exports.sendStream = function (req, res) {
 
@@ -13,8 +13,11 @@ module.exports.sendStream = function (req, res) {
     const ffmpeg = child_process.spawn('ffmpeg', [
         '-fflags', '+igndts', '-an', '-reorder_queue_size', '5', '-max_delay', '5', '-rtsp_transport', 'tcp',
         '-i', path, '-framerate', '25',
-        '-an', '-c:v', 'copy', '-f', 'mp4', '-movflags', '+frag_keyframe+empty_moov+default_base_moof', '-metadata', 'title="media source extensions"', 'pipe:1'
-    ])
+        '-an', '-c:v', 'copy', '-thread_type', 'frame', '-f', 'mp4', '-movflags', '+frag_keyframe+empty_moov+default_base_moof', '-metadata', 'title="media source extensions"', 'pipe:1'
+    ], {
+        shell: true,
+        detached: false,
+    })
 
     ffmpeg.stderr.setEncoding('utf8');
     ffmpeg.stderr.on('data', (data) => {
