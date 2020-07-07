3b88dc2a0225528c87e20ee1c78c2c540b15a5b5
tothadi <Oakerator66>
2020-07-07 13:16:48 +0200

    removed shell

--- api/controllers/stream.js
+++ api/controllers/stream.js
@@ -14,10 +14,7 @@ module.exports.sendStream = function (req, res) {
         '-fflags', '+igndts', '-an', '-reorder_queue_size', '5', '-max_delay', '5', '-rtsp_transport', 'tcp',
         '-i', path, '-framerate', '25',
         '-an', '-c:v', 'copy', '-thread_type', 'frame', '-f', 'mp4', '-movflags', '+frag_keyframe+empty_moov+default_base_moof', '-metadata', 'title="media source extensions"', 'pipe:1'
-    ], {
-        shell: true,
-        detached: false,
-    })
+    ])
 
     ffmpeg.stderr.setEncoding('utf8');
     ffmpeg.stderr.on('data', (data) => {
