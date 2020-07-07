e362d089f0b541e1e1ca93c47252d1d0e51fb2f4
tothadi <Oakerator66>
2020-07-07 10:40:40 +0200

    added max delay

--- api/controllers/stream.js
+++ api/controllers/stream.js
@@ -1,9 +1,6 @@
 const child_process = require('child_process'),
     path = process.env.StreamURI//'./cars.mp4'//
 
-
-
-
 module.exports.sendStream = function (req, res) {
 
     res.writeHead(200, {
@@ -13,9 +10,9 @@ module.exports.sendStream = function (req, res) {
     })
 
     const ffmpeg = child_process.spawn('ffmpeg', [
-        '-fflags', '+igndts', '-reorder_queue_size', '5', '-rtsp_transport', 'tcp',
+        '-fflags', '+igndts', '-reorder_queue_size', '5', '-max_delay', '5', '-rtsp_transport', 'tcp',
         '-i', path, '-framerate', '25',
-        '-an', '-c:v', 'copy', '-f', 'h264', '-movflags', '+frag_keyframe+empty_moov+default_base_moof', '-metadata', 'title="media source extensions"', 'pipe:1'
+        '-an', '-c:v', 'copy', '-f', 'mp4', '-movflags', '+frag_keyframe+empty_moov+default_base_moof', '-metadata', 'title="media source extensions"', 'pipe:1'
     ])
 
     ffmpeg.stderr.setEncoding('utf8');
