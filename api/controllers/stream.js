63a4eba5ae2cf7990b5415caa6f6ad1ad08517cb
tothadi <Oakerator66>
2020-07-08 09:09:51 +0200

    fluent deleted

--- api/controllers/stream.js
+++ api/controllers/stream.js
@@ -1,7 +1,6 @@
 
 const child_process = require('child_process'),
-    path = process.env.StreamURI,//'./cars.mp4'//
-    ffmpeg = require('fluent-ffmpeg')
+    path = process.env.StreamURI//'./cars.mp4'//
 
 module.exports.sendStream = function (req, res) {
 
@@ -11,54 +10,22 @@ module.exports.sendStream = function (req, res) {
         'Content-Type': 'video/mp4'
     })
 
-    ffmpeg()
-        .input(path)
-        .native()
-        .noAudio()
-        .output(stream)
-        .noAudio()
-        .videoCodec('copy')
-        .format('mp4')
-        .outputOptions([
-            '-movflags', '+frag_keyframe+empty_moov+default_base_moof',
-            '-metadata', 'title="media source extensions"',
-            'pipe:1'
-        ])
-        .on('start', function (commandLine) {
-            console.log('Spawned Ffmpeg with command: ' + commandLine);
-        })
-        .on('codecData', function (data) {
-            console.log('Input is ' + data.audio + ' audio ' +
-                'with ' + data.video + ' video');
-        })
-        .on('stderr', function (stderrLine) {
-            console.log('Stderr output: ' + stderrLine);
-        })
-        .on('error', function (err, stdout, stderr) {
-            console.log('Cannot process video: ' + err.message);
-        })
-        .pipe(res)
-    /*const ffmpeg = child_process.spawn('ffmpeg', [
+    const ffmpeg = child_process.spawn('ffmpeg', [
         '-fflags', '+igndts', '-an', '-rtsp_transport', 'tcp',
         '-i', path,
         '-an', '-c:v', 'copy', '-f', 'mp4', '-movflags', '+frag_keyframe+empty_moov+default_base_moof', '-metadata', 'title="media source extensions"', 'pipe:1'
-    ]).then(() => {
-
-    })
+    ])
 
     ffmpeg.stderr.setEncoding('utf8');
     ffmpeg.stderr.on('data', (data) => {
         console.log(data)
     })
 
-    ffmpeg.stdout.on('data', data => {
-        data.pipe(res)
-    })*/
-
-
+    ffmpeg.stdout.pipe(res)
 
     req.on('close', () => {
         res.destroy()
+        console.log('connection closed')
     })
 
 }
