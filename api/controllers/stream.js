5807a9e2d4162398519dd0aa94b11928a7133653
tothadi <Oakerator66>
2020-07-08 10:05:56 +0200

    copy deleted

--- api/controllers/stream.js
+++ api/controllers/stream.js
@@ -13,7 +13,7 @@ module.exports.sendStream = function (req, res) {
     const ffmpeg = child_process.spawn('ffmpeg', [
         '-fflags', '+igndts', '-an', '-rtsp_transport', 'tcp',
         '-i', path, '-vf', 'select=concatdec_select',
-        '-an', '-c:v', 'copy', '-f', 'mp4', '-movflags', '+frag_keyframe+empty_moov+default_base_moof', '-metadata', 'title="media source extensions"', 'pipe:1'
+        '-an', '-f', 'mp4', '-movflags', '+frag_keyframe+empty_moov+default_base_moof', '-metadata', 'title="media source extensions"', 'pipe:1'
     ])
 
     ffmpeg.stderr.setEncoding('utf8');
