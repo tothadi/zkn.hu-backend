68e778f9a93169feebdb7c5cb00796b8c56c162e
tothadi <Oakerator66>
2020-07-08 08:03:38 +0200

    changing pipe

--- api/controllers/stream.js
+++ api/controllers/stream.js
@@ -17,14 +17,18 @@ module.exports.sendStream = function (req, res) {
         '-fflags', '+igndts', '-an', '-rtsp_transport', 'tcp',
         '-i', path,
         '-an', '-c:v', 'copy', '-f', 'mp4', '-movflags', '+frag_keyframe+empty_moov+default_base_moof', '-metadata', 'title="media source extensions"', 'pipe:1'
-    ])
+    ]).then(() => {
+        
+    })
 
     ffmpeg.stderr.setEncoding('utf8');
     ffmpeg.stderr.on('data', (data) => {
         console.log(data)
     })
 
-    ffmpeg.stdout.pipe(res)
+    ffmpeg.stdout.on('data', data => {
+        data.pipe(res)
+    })
 
     req.on('close', () => {
         ffmpeg.kill()
